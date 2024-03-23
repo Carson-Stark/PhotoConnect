cd5079d3494c89df407d8f478174a9cef0cdf599
Om <031230om@gmail.com>
2024-03-23 15:30:01 -0500

    can upload photos into firebase storage; need to connect each user to images in firestore

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
+import { getStorage, ref } from "firebase/storage";
 
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -25,3 +25,4 @@ export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
 // Initialize Cloud Firestore and get a reference to the service
 export const FIREBASE_DB = getFirestore(FIREBASE_APP);
+export const FIREBASE_STORAGE = getStorage(FIREBASE_APP);
--- app/screens/AddImage.tsx
+++ app/screens/AddImage.tsx
@@ -1,13 +1,63 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { View, Text, StyleSheet, Button, Pressable } from 'react-native';
-import { FIREBASE_AUTH } from '../../FirebaseConfig';
+import { FIREBASE_AUTH, FIREBASE_DB, FIREBASE_STORAGE } from '../../FirebaseConfig';
 import { useNavigation } from '@react-navigation/native';
 import Home from './Home';
 
+// import 'firebase/compat/firestore';
+// import 'firebase/compat/storage';
+// import firebase from 'firebase/compat/app';
 
+import {getDownloadURL, listAll, ref, uploadBytes} from "firebase/storage"
+import { v4 } from "uuid"
+import { collapseTextChangeRangesAcrossMultipleVersions } from 'typescript';
 
 const AddImage = () => {
     const navigation = useNavigation();
+
+    const [image, setImage] = useState('');
+    const [imageURL, setImageURL] = useState([]);
+
+    // const handleChange = (e)=>{
+    //     if(e.target.files[0]){
+    //         setImage(e.target.files[0])
+    //     }
+    // }
+
+    const handleUpload = ()=>{
+        const imgRef = ref(FIREBASE_STORAGE, `files/${v4()}`)
+        uploadBytes(imgRef, image)
+        // const uploadTask = ref(`images/test`).put(image);
+        // uploadTask.on(
+        //     "state changed",
+        //     () => {
+        //         FIREBASE_STORAGE
+        //             .ref("images")
+        //             .child()
+        //             .getDownloadURL()
+        //             .then(url => {
+        //                 FIREBASE_DB.collection("posts").add({
+        //                     imageURL: url
+        //                 })
+        //             })
+        //     }
+        // )
+        // // setcaption();
+        // setImage(null);
+    }
+
+    useEffect(()=>{
+        listAll(ref(FIREBASE_STORAGE, "files")).then(imgs=>{
+            console.log(imgs),
+            imgs.items.forEach(val => {
+                    getDownloadURL(val).then(url=>{
+                        setImageURL(data=>[...data, url])
+                    })
+            });
+        })
+    }, [])
+
+    console.log(imageURL, "imageURL");
 
     return (
         <View style={styles.container}>
@@ -15,6 +65,14 @@ const AddImage = () => {
             <Pressable style={styles.button} onPress={() => navigation.goBack()}>
                 <Text style={{ color: 'white', padding: 10 }}>Back</Text>
             </Pressable>
+            {/* <h2>Add New Post </h2>
+            <input type='file' onChange={()=>{handleChange}}/>
+            <Button onClick={handleUpload}>
+                ADD POST
+            </Button> */}
+            <input type='file' onChange={(e)=>{ setImage(e.target.files[0])}}/>
+            <button onClick={handleUpload}>Upload</button>
+
         </View>
     );
 };
